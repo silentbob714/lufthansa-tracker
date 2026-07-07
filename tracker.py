b166480364f96715fb4d18f535d6520f9bcd2f29
silentbob714 <74814333+silentbob714@users.noreply.github.com>
2026-07-07 03:57:52 -0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,11 +1,19 @@
 import os
 import requests
-from datetime import datetime, timezone
 
 WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
+username = os.environ["OPENSKY_USERNAME"]
+password = os.environ["OPENSKY_PASSWORD"]
+
+response = requests.get(
+    "https://opensky-network.org/api/states/all",
+    auth=(username, password),
+    timeout=30
+)
+
 payload = {
-    "content": f"✈ Tracker check: {datetime.now(timezone.utc)}"
+    "content": f"OpenSky Status: {response.status_code}"
 }
 
 requests.post(WEBHOOK, json=payload)
