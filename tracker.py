fc58ce90c3fb12db059a9fb3d3c336b51b8f5e16
silentbob714 <74814333+silentbob714@users.noreply.github.com>
2026-07-07 03:29:08 -0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,10 +1,11 @@
 import os
 import requests
+from datetime import datetime, timezone
 
 WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
 payload = {
-    "content": "✈ Lufthansa Tracker test successful."
+    "content": f"✈ Tracker check: {datetime.now(timezone.utc)}"
 }
 
 requests.post(WEBHOOK, json=payload)
